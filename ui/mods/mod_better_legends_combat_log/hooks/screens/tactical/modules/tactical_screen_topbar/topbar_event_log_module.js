7627f8d7ae0e1f270d83af19278bf2a1ebb62321
mnshdw <1048669+mnshdw@users.noreply.github.com>
2026-03-07 22:53:22 +0100

    New state: retracted, for maximum screen estate

--- ui/mods/mod_better_legends_combat_log/hooks/screens/tactical/modules/tactical_screen_topbar/topbar_event_log_module.js
+++ ui/mods/mod_better_legends_combat_log/hooks/screens/tactical/modules/tactical_screen_topbar/topbar_event_log_module.js
@@ -1,8 +1,11 @@
 TacticalScreenTopbarEventLogModule.prototype.createDIV = function (_parentDiv) {
 	this.mNormalHeight = '13.0rem';
 	this.mExtendedHeight = '80.0rem';
+	this.mRetractedHeight = '3.5rem';
 	this.mExpandDelay = 200;
 	this.mMaxVisibleEntries = 150;
+
+	this.mLogState = 'normal';
 
 	var grandpa = _parentDiv.parent();
 	_parentDiv.css('opacity', '0');
@@ -12,6 +15,8 @@ TacticalScreenTopbarEventLogModule.prototype.createDIV = function (_parentDiv) {
 	var width = grandpa.parent().width() / 3;
 	newlog.css('width', width);
 	newlog.css('background-size', newlog.width() + " " + newlog.height());
+
+	this.mNewLogContainer = newlog;
 
 	var self = this;
 
@@ -59,7 +64,7 @@ TacticalScreenTopbarEventLogModule.prototype.createDIV = function (_parentDiv) {
 	var layout = $('<div class="l-expand-button"/>');
 	this.mContainer.append(layout);
 	this.ExpandButton = layout.createImageButton(Path.GFX + Asset.BUTTON_OPEN_EVENTLOG, function () {
-		self.expand(!self.mIsExpanded);
+		self.cycleLogState();
 	}, '', 6);
 	//this.ExpandButton.css('z-index', '9999999');
 	this.expand(false);
@@ -89,4 +94,85 @@ TacticalScreenTopbarEventLogModule.prototype.changeFontSize = function (_fontSiz
 		this.mCurrentFontSizeClass = cssClass;
 		this.mEventLogsContainerLayout.addClass(cssClass);
 	}
+};
+
+TacticalScreenTopbarEventLogModule.prototype.cycleLogState = function () {
+	var nextState;
+	switch (this.mLogState) {
+		case 'retracted': nextState = 'normal'; break;
+		case 'normal': nextState = 'expanded'; break;
+		case 'expanded': nextState = 'retracted'; break;
+	}
+	this.setLogState(nextState);
+};
+
+TacticalScreenTopbarEventLogModule.prototype.setLogState = function (_state) {
+	if (this.mLogState === _state) return;
+
+	var previousState = this.mLogState;
+	var self = this;
+	this.mLogState = _state;
+	this.mIsExpanded = (_state === 'expanded');
+
+	// Finish any pending animations
+	this.mEventsListContainer.velocity("finish", true);
+
+	if (_state === 'retracted') {
+		// Snap to retracted instantly
+		this.mNewLogContainer.velocity("finish", true);
+		this.mNewLogContainer.css({ 'height': '5.5rem', 'overflow': 'hidden' });
+		this.mEventLogsContainerLayout.css('height', this.mRetractedHeight);
+		this.mEventsListContainer.css({ 'height': this.mRetractedHeight, 'padding-bottom': '0' });
+		this.mEventsListContainer.showListScrollbar(false);
+		this.mEventsListContainer.trigger('update', true);
+		this.mEventsListContainer.scrollListToBottom();
+		this.ExpandButton.changeButtonImage(Path.GFX + Asset.BUTTON_OPEN_EVENTLOG);
+		return;
+	}
+
+	if (previousState === 'retracted') {
+		// Restore container from retracted instantly
+		this.mNewLogContainer.velocity("finish", true);
+		this.mNewLogContainer.css({ 'height': '15.1rem', 'overflow': '' });
+		this.mEventLogsContainerLayout.css('height', this.mNormalHeight);
+		this.mEventsListContainer.css('height', this.mNormalHeight);
+	}
+
+	if (_state === 'normal') {
+		// Already at normal sizes (either from retracted restore above, or from expanded finish)
+		this.mEventsListContainer.css({ 'height': this.mNormalHeight, 'padding-bottom': '0' });
+		this.mEventsListContainer.showListScrollbar(false);
+		this.mEventsListContainer.trigger('update', true);
+		this.mEventsListContainer.scrollListToBottom();
+		this.ExpandButton.changeButtonImage(Path.GFX + Asset.BUTTON_OPEN_EVENTLOG);
+		return;
+	}
+
+	// Normal => Expanded: animate the list only (container stays at 15.1rem)
+	this.mEventsListContainer.velocity(
+		{ height: this.mExtendedHeight },
+		{
+			easing: 'linear',
+			duration: this.mExpandDelay,
+			begin: function () {
+				self.mEventsListContainer.scrollListToElement();
+				self.mEventsListContainer.css({ 'padding-bottom': '1.8rem' });
+			},
+			complete: function () {
+				self.mEventsListContainer.trigger('update', true);
+				self.mEventsListContainer.scrollListToElement();
+				self.mEventsListContainer.showListScrollbar(true);
+				self.ExpandButton.changeButtonImage(Path.GFX + Asset.BUTTON_CLOSE_EVENTLOG);
+			}
+		}
+	);
+};
+
+TacticalScreenTopbarEventLogModule.prototype.expand = function (_value) {
+	var targetState = _value ? 'expanded' : 'normal';
+	if (this.mLogState === targetState) {
+		this.mEventsListContainer.showListScrollbar(_value);
+		return;
+	}
+	this.setLogState(targetState);
 };
